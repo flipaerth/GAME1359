84f01daaae192e93bc0ece3fdb52b1acdd2c41fb
flipaerth <austinozan@gmail.com>
2021-09-28 19:42:58 -0500

    finish

---
+++ Code/GAME 1359/Assets/Assignments/WeekThree.cs
@@ -0,0 +1,15 @@
+﻿/*
+Using the videos above as a reference, you are to use Unity/C# to build a simple 3D platformer
+for this week's assignment; however, unlike in the video, also ensure that:
+
+The character only jumps when touching the ground
+(hint: raycasts, tags, and variables will help you here).
+
+You include a variable, available in the Inspector,
+to change the Player's move speed.
+
+You don't need to worry about animations or level design
+for this assignment - just a character that can move and jump.
+
+Next week, we'll add stuff to improve on this :)
+ */
---
+++ Code/GAME 1359/Assets/Scripts/Week 3/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Public Variable for movement speed - adjusted in editor - set to 1 by default
+    public float playerSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         Input Variables
+         */
+
+        // Horizontal Axis
+        float h = Input.GetAxis("Horizontal");
+        // Vertical Axis
+        float v = Input.GetAxis("Vertical");
+
+        /*
+         Movement
+         */
+        // Time.deltaTime = converts to real time, rather than based on every frame
+        transform.position = new Vector3(transform.position.x + h * Time.deltaTime * playerSpeed,
+                                         transform.position.y,
+                                         transform.position.z + v * Time.deltaTime * playerSpeed);
+    }
+}
